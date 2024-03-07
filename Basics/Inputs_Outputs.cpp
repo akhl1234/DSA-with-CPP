e133e67836e389fddcf653a200ce51f5aab2028d
akhl1234 <shameer.alam2512@outlook.com>
2024-03-07 20:57:52 +0530

    Mistake correction In Input_Output cpp files

--- Basics/Inputs_Outputs.cpp
+++ Basics/Inputs_Outputs.cpp
@@ -9,7 +9,7 @@ int main()
   cout << "my name is " << name << " and i am " << Age << " Years old--" << endl;
   cout << "Thank You.";
 
-  // lets explore addition method ny using input as we want.
+  // lets explore Int method ny using input as we want.
   int X, Y;
   cin >> X >> Y;
   cout << "the value of X is: " << X << endl
