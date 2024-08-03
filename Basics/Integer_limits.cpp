e6ee1375a1ea1ec505d22f0706de5282597baf60
Akhlaque Ansari <124654189+akhl1234@users.noreply.github.com>
2024-08-03 05:16:59 +0000

    Add program to check integer limits

---
+++ Basics/Integer_limits.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+#include <climits>  // For integer limits
+
+using namespace std; 
+
+int main() {
+    cout << "\nCheck the upper and lower limits of integers:\n";
+    cout << "---------------------------------------------\n";
+
+    cout << "Maximum limit of int data type: " << INT_MAX << endl;
+    cout << "Minimum limit of int data type: " << INT_MIN << endl;
+    cout << "Maximum limit of unsigned int data type: " << UINT_MAX << endl;
+    cout << "Maximum limit of long long data type: " << LLONG_MAX << endl;
+    cout << "Minimum limit of long long data type: " << LLONG_MIN << endl;
+    cout << "Maximum limit of unsigned long long data type: " << ULLONG_MAX << endl;
+    cout << "Bits contained in char data type: " << CHAR_BIT << endl;
+    cout << "Maximum limit of char data type: " << CHAR_MAX << endl;
+    cout << "Minimum limit of char data type: " << CHAR_MIN << endl;
+    cout << "Maximum limit of signed char data type: " << SCHAR_MAX << endl;
+    cout << "Minimum limit of signed char data type: " << SCHAR_MIN << endl;
+    cout << "Maximum limit of unsigned char data type: " << UCHAR_MAX << endl;
+    cout << "Minimum limit of short data type: " << SHRT_MIN << endl;
+    cout << "Maximum limit of short data type: " << SHRT_MAX << endl;
+    cout << "Maximum limit of unsigned short data type: " << USHRT_MAX << endl;
+
+    cout << endl; 
+
+    return 0;
+}
